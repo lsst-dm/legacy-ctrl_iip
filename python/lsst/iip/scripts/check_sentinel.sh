d4bf72c0eb03ea09c1dda4cf5ecfcbcddf31ca2c
Jim Parsons <jbparson@illinois.edu>
2016-06-06 14:10:09 +0000

    comments...minor ordering change

--- python/lsst/iip/scripts/check_sentinel.sh
+++ python/lsst/iip/scripts/check_sentinel.sh
@@ -1,6 +1,6 @@
 FLOG=~/xfer_dir/rcv_logg.test
 while true; do
-  if [ -f ~/xfer_dir/7entinel.test ]
+  if [ -f ~/xfer_dir/sentinel.test ]
     then
       date +"%Y-%m-%d %H:%M:%S.%5N" >> $FLOG
       echo "Found sentinel file at this time " >> $FLOG
