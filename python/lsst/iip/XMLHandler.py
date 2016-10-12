1ce8d7eb735384269b2bfd216228b48b8fdc9714
Htut Khine <htutkhwin@gmail.com>
2016-10-12 13:56:05 -0500

    recursive encodeXML and decodeXML added.

--- python/lsst/iip/XMLHandler.py
+++ python/lsst/iip/XMLHandler.py
@@ -1,7 +1,6 @@
 from lxml import etree
 from const import *
 from copy import deepcopy 
-import yaml
 import sys
 
 
@@ -33,51 +32,78 @@ class XMLHandler:
 
     def encodeXML(self, dictValue):
         """ Encode python dictionary into XML
-            Has to convert ACK_BOOL to "true" so that it works with xml boolean
-            :param pydict: python dictionary
-            :type pydict: dict
+            Has to convert ACK_BOOL to "ack_bool_true" so that it works with conditional
+            :param dictValue: python dictionary to encode XML
+            :type dictValue: dict
         """
         pydict = deepcopy(dictValue)
         root = etree.Element("messageDict")
-        msg = etree.Element("message", MSG_TYPE=pydict["MSG_TYPE"]) 
+        msg = etree.Element("message", MSG_TYPE=pydict["MSG_TYPE"])
         root.append(msg)
-
-        for kee, value in pydict.items(): 
-            if kee != "MSG_TYPE" and type(value) != bool:
-                keenode = etree.Element(kee)
-                msg.append(keenode)
-                keenode.text = str(value)
-            elif type(value) == bool: 
-                pydict[kee] = str(value).lower()
-                if msg.find("ACK_BOOL") is not None: 
-                    ack_bool = msg.find("ACK_BOOL")
-                    ack_bool.set(kee.lower() + "_" + pydict[kee], pydict[kee]) 
-                else: 
-                    ack = str(pydict["ACK_BOOL"]).lower()
-                    ack_bool = etree.Element("ACK_BOOL")
-                    ack_bool.set("ack_bool_" + ack, ack) 
-                    ack_bool.set(kee.lower() + "_" + pydict[kee], pydict[kee]) 
-                    msg.append(ack_bool) 
+        subTree = self.recursive_encodeXML(msg, pydict)
         return root
+
+    def recursive_encodeXML(self, node, msgDict): 
+        """ Recursively encode the XML dictionary
+            :param node: empty XML node to add elements
+            :param msgDict: python dictionary to convert values from
+            :type node: lxml Element
+            :type msgDict: dict
+        """ 
+        for kee, val in msgDict.items(): 
+            if kee != "MSG_TYPE": 
+                subNode = etree.SubElement(node, kee)
+                if type(val) != dict:
+                    if kee == "ACK_BOOL": 
+                        boolean = str(val).lower() 
+                        subNode.set("ack_bool_" + boolean, boolean)
+                    else:
+                        subNode.text = str(val)
+                else:
+                    subsubNode = self.recursive_encodeXML(subNode, val)
+        return self.get_parent(node) 
+
+    def get_parent(self, node): 
+        """ Recursively calls the parent node of current element
+            :param node: XML root node to call its parent
+            :type node: lxml Element
+        """ 
+        if node.getparent() is None: 
+            return node
+        else:
+            return self.get_parent(node.getparent())
 
     def decodeXML(self, rootNode):
         """ Decode XML tree to python dictionary
             Has to convert the "true" back to python boolean True
-            :param rootNode: XML root node
+            :param rootNode: XML root node to conver to python dictionary
             :type rootNode: lxml etree
         """
         pydict = {}
-        pydict["MSG_TYPE"] = rootNode[0].get("MSG_TYPE") 
-        for node in rootNode.iter():
-            if node.text != None: 
-                pydict[node.tag] = node.text
-            elif node.tag == "ACK_BOOL": 
-                attr = node.attrib
-                for kee,value in attr.items():
-                    key = kee.replace("_" + value,"")
-                    val = True if value == "true" else False
-                    pydict[key.upper()] = val
-        return pydict
+        message = rootNode.find("message")
+        xmlDict = self.recursive_decodeXML(rootNode, pydict)
+        xmlDict["message"]["MSG_TYPE"] = next(val for kee, val in message.attrib.iteritems() if kee.startswith("MSG_TYPE"))
+        return xmlDict["message"]
+
+    def recursive_decodeXML(self, rootnode, msgDict): 
+        """ Recursively decode XML back to python dictionary
+            :param rootnode: XML root node to convert to python dictionary
+            :param msgDict: empty python dictionary to add elements
+            :type rootnode: lxml Element
+            :type msgDict: dict
+        """
+        for node in rootnode:  
+            if len(node) != 0:  
+                smallDict = {} 
+                self.recursive_decodeXML(node, smallDict)
+                msgDict[node.tag] = smallDict
+            else:
+                if len(node.attrib) != 0: 
+                    ack_bool = next(val for kee, val in node.attrib.iteritems() if kee.startswith("ack_bool"))
+                    msgDict[node.tag] = bool(ack_bool)
+                else:
+                    msgDict[node.tag] = node.text
+        return msgDict
 
     def tostring(self, rootNode):
         """ Changes the XML tree to string
