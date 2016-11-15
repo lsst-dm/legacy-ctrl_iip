2c0532e105ec74c14db394dbeb718fa7f4d5d363
Jim Parsons <jbparson@illinois.edu>
2016-11-15 16:11:46 +0000

    Removed debug statements

--- python/lsst/iip/Scoreboard.py
+++ python/lsst/iip/Scoreboard.py
@@ -22,9 +22,6 @@ class Scoreboard:
     SCOREBOARD_TYPE = None
 
     def __init__(self, scoreboard_type, file=None):
-        print "The scoreboard arg is "
-        print scoreboard_type
-        print "Done printing scoreboard arg is "
         if file == None:
             file = 'ForemanCfgTest.yaml'
         try:
@@ -42,9 +39,6 @@ class Scoreboard:
                 break
 
 #        self.SCOREBOARD_TYPE = (key for key, value in self.SCBDS.iteritems() if value == scoreboard_type)
-        print "Printing value of self.SCOREBOARD_TYPE"
-        print self.SCOREBOARD_TYPE
-        print "Done printing value of self.SCOREBOARD_TYPE"
         broker_address = self.cdm['ROOT']['BASE_BROKER_ADDR']
         name = self.cdm['ROOT']['BASE_BROKER_NAME']
         passwd = self.cdm['ROOT']['BASE_BROKER_PASSWD']
