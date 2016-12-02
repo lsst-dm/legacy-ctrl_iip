75b0d4cb7060abbedba8853c50db3d8daa2d58d2
hwin16 <htutkhwin@gmail.com>
2016-12-02 22:52:10 +0000

    initial working prototype.

--- python/lsst/iip/ocs/CommandListener.cpp
+++ python/lsst/iip/ocs/CommandListener.cpp
@@ -11,18 +11,15 @@ using namespace DDS;
 using namespace dm; 
 using namespace AmqpClient;
 
-extern "C" { 
-    OS_API_EXPORT
-    int dm_command_controller(); 
-} 
-
-CommandListener::CommandListener(string url): OCS_Bridge(url) { 
+CommandListener::CommandListener(string CommandEntity) : OCS_Bridge(CommandEntity) { 
     mgr = SAL_dm(); 
 
+    cmdEntity = CommandEntity; 
     command_args = new ocs_thread_args; 
     command_args->dmgr = mgr; 
-    command_args->ocsAmqp = ocs_consume_args->amqpObj;
-    command_args->q = ocs_consume_args->queue_name;  
+    command_args->ocsAmqp = ocs_publisher;
+    command_args->q = OCS_PUBLISH;  
+    command_args->entity = cmdEntity; 
     setup_ocs_consumer();
 } 
 
@@ -39,6 +36,7 @@ void *CommandListener::run_ocs_consumer(void *pargs) {
     SAL_dm mgr = params->dmgr; 
     Channel::ptr_t publisher = params->ocsAmqp; 
     string queue = params->q; 
+    string command_entity = params->entity; 
     
     os_time delay_10ms = {0, 10000000}; 
 
@@ -54,28 +52,28 @@ void *CommandListener::run_ocs_consumer(void *pargs) {
     mgr.salProcessor("dm_command_exitControl");
     mgr.salProcessor("dm_command_abort");
     
-    cout << "=== dm command controller ready" << endl; 
+    cout << "=== dm COMMAND controller ready" << endl; 
 
     while (1) { 
-	dm_start(cmdId, timeout, delay_10ms, publisher, queue, mgr); 
-	dm_stop(cmdId, timeout, delay_10ms, publisher, queue, mgr);  
-	dm_enable(cmdId, timeout, delay_10ms, publisher, queue, mgr);  
-	dm_disable(cmdId, timeout, delay_10ms, publisher, queue, mgr);  
-	dm_enterControl(cmdId, timeout, delay_10ms, publisher, queue, mgr);  
-	dm_standby(cmdId, timeout, delay_10ms, publisher, queue, mgr);  
-	dm_exitControl(cmdId, timeout, delay_10ms, publisher, queue, mgr);  
-	dm_abort(cmdId, timeout, delay_10ms, publisher, queue, mgr);  
+	dm_start(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr); 
+	dm_stop(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr);  
+	dm_enable(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr);  
+	dm_disable(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr);  
+	dm_enterControl(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr);  
+	dm_standby(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr);  
+	dm_exitControl(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr);  
+	dm_abort(cmdId, timeout, delay_10ms, publisher, queue, command_entity, mgr);  
     }
     mgr.salShutdown(); 
     return 0;
 }  
 
-void CommandListener::dm_start(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_start(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_startC SALInstance; 
     
     cmdId = mgr.acceptCommand_start(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== START Command " << endl;
 	cout << "configuration: " << SALInstance.configuration << endl; 
 	ostringstream message; 
@@ -85,12 +83,12 @@ void CommandListener::dm_start(int cmdId, int timeout, os_time delay_10ms, Chann
     os_nanoSleep(delay_10ms);
 } 
 
-void CommandListener::dm_stop(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_stop(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_stopC SALInstance; 
     
     cmdId = mgr.acceptCommand_stop(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== STOP Command " << endl;
 	cout << "state: " << SALInstance.state << endl; 
 	ostringstream message; 
@@ -100,12 +98,12 @@ void CommandListener::dm_stop(int cmdId, int timeout, os_time delay_10ms, Channe
     os_nanoSleep(delay_10ms);
 } 
 
-void CommandListener::dm_enable(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_enable(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_enableC SALInstance; 
     
     cmdId = mgr.acceptCommand_enable(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== ENABLE Command " << endl;
 	cout << "state: " << SALInstance.state << endl; 
 	ostringstream message; 
@@ -115,12 +113,12 @@ void CommandListener::dm_enable(int cmdId, int timeout, os_time delay_10ms, Chan
     os_nanoSleep(delay_10ms);
 } 
 
-void CommandListener::dm_disable(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_disable(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_disableC SALInstance; 
     
     cmdId = mgr.acceptCommand_disable(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== DISABLE Command " << endl;
 	cout << "state: " << SALInstance.state << endl; 
 	ostringstream message; 
@@ -130,12 +128,12 @@ void CommandListener::dm_disable(int cmdId, int timeout, os_time delay_10ms, Cha
     os_nanoSleep(delay_10ms);
 } 
 
-void CommandListener::dm_enterControl(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_enterControl(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_enterControlC SALInstance; 
     
     cmdId = mgr.acceptCommand_enterControl(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== ENTERCONTROL Command " << endl;
 	cout << "state: " << SALInstance.state << endl; 
 	ostringstream message; 
@@ -145,12 +143,12 @@ void CommandListener::dm_enterControl(int cmdId, int timeout, os_time delay_10ms
     os_nanoSleep(delay_10ms);
 } 
 
-void CommandListener::dm_standby(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_standby(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_standbyC SALInstance; 
     
     cmdId = mgr.acceptCommand_standby(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== STANDBY Command " << endl;
 	cout << "state: " << SALInstance.state << endl; 
 	ostringstream message; 
@@ -160,12 +158,12 @@ void CommandListener::dm_standby(int cmdId, int timeout, os_time delay_10ms, Cha
     os_nanoSleep(delay_10ms);
 } 
 
-void CommandListener::dm_exitControl(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_exitControl(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_exitControlC SALInstance; 
     
     cmdId = mgr.acceptCommand_exitControl(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== EXITCONTROL Command " << endl;
 	cout << "state: " << SALInstance.state << endl; 
 	ostringstream message; 
@@ -175,12 +173,12 @@ void CommandListener::dm_exitControl(int cmdId, int timeout, os_time delay_10ms,
     os_nanoSleep(delay_10ms);
 } 
 
-void CommandListener::dm_abort(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, SAL_dm mgr) { 
+void CommandListener::dm_abort(int cmdId, int timeout, os_time delay_10ms, Channel::ptr_t publisher, string queue, string cmdEntity, SAL_dm mgr) { 
     dm_command_abortC SALInstance; 
     
     cmdId = mgr.acceptCommand_abort(&SALInstance); 
     
-    if (cmdId > 0) { 
+    if (cmdId > 0 && cmdEntity == SALInstance.device) { 
 	cout << "== ABORT Command " << endl;
 	cout << "state: " << SALInstance.state << endl; 
 	ostringstream message; 
@@ -191,7 +189,7 @@ void CommandListener::dm_abort(int cmdId, int timeout, os_time delay_10ms, Chann
 }
  
 int main() { 
-    CommandListener cmd("amqp://ocs:ocs@141.142.208.241/%2fbridge"); 
+    CommandListener cmd("PromptProcessor"); 
     pthread_exit(NULL); 
     return 0;
 } 
