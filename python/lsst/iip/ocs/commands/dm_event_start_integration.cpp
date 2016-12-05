f5f9d7201576416fe4a8a02875c8c23a46135334
hwin16 <htutkhwin@gmail.com>
2016-12-05 17:09:57 +0000

    OCS commands updated.

---
+++ python/lsst/iip/ocs/commands/dm_event_start_integration.cpp
@@ -0,0 +1,63 @@
+
+/*
+ * This file contains the implementation for the 'dm_Event' executable.
+ *
+ ***/
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <unistd.h>
+#include "SAL_dm.h"
+#include "ccpp_sal_dm.h"
+#include "os.h"
+
+#include "example_main.h"
+
+using namespace DDS;
+using namespace dm;
+
+/* entry point exported and demangled so symbol can be found in shared library */
+extern "C"
+{
+  OS_API_EXPORT
+  int dm_Event(char *alias , char *message);
+}
+
+int dm_Event(char *alias, char *message)
+{
+  
+  int priority = SAL__EVENT_INFO;
+
+#ifdef SAL_SUBSYSTEM_ID_IS_KEYED
+  int dmID = 1;
+  SAL_dm mgr = SAL_dm(dmID);
+#else
+  SAL_dm mgr = SAL_dm();
+#endif
+
+  // generate event
+  mgr.logEvent(message, priority);
+  cout << "=== Event " << alias << " generated = " << message << endl;
+  sleep(1);
+
+  /* Remove the DataWriters etc */
+  mgr.salShutdown();
+
+  return 0;
+}
+
+
+int OSPL_MAIN (int argc, char *argv[])
+{
+  if (argc < 3) {
+     cout << "A alias + message text is required at a minimum" << endl;
+     exit(1);
+  }
+
+  ostringstream message; 
+  message << "{MSG_TYPE: START_INTEGRATION, IMAGE_ID: " << argv[2] << ", TARGET_DESTINATION: " << argv[3] << "}"; 
+  return dm_Event (argv[1], const_cast<char *>(message.str().c_str()));
+}
+
+
