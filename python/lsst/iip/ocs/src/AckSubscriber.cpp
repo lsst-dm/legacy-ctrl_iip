78844cf4cbe6ec3a1644927d62388430ccf26c0e
hwin16 <htutkhwin@gmail.com>
2017-03-29 19:50:49 +0000

    AckSubscriber works for every device.

--- python/lsst/iip/ocs/src/AckSubscriber.cpp
+++ python/lsst/iip/ocs/src/AckSubscriber.cpp
@@ -1,7 +1,9 @@
+#include <functional> 
 #include <ctime> 
 #include <string.h>
 #include <iostream> 
 #include <yaml-cpp/yaml.h>
+#include <boost/variant.hpp> 
 #include "SAL_archiver.h"
 #include "SAL_catchuparchiver.h" 
 #include "SAL_processingcluster.h"
@@ -13,17 +15,38 @@
 using namespace std; 
 using namespace YAML; 
 
-typedef salReturn (SAL_archiver::*funcptr)(int, salLONG, salLONG, char *);
 
-map<string, pair<string,funcptr>> action_handler = {
-    {"START_ACK", make_pair("archiver_command_start", &SAL_archiver::ackCommand_start)},
-    {"STOP_ACK", make_pair("archiver_command_stop", &SAL_archiver::ackCommand_stop)},
-    {"ENABLE_ACK", make_pair("archiver_command_enable", &SAL_archiver::ackCommand_enable)},
-    {"DISABLE_ACK", make_pair("archiver_command_disable", &SAL_archiver::ackCommand_disable)},
-    {"ENTERCONTROL_ACK", make_pair("archiver_command_enterControl", &SAL_archiver::ackCommand_enterControl)},
-    {"STANDBY_ACK", make_pair("archiver_command_standby", &SAL_archiver::ackCommand_standby)}, 
-    {"OFFLINE_ACK", make_pair("archiver_command_exitControl", &SAL_archiver::ackCommand_exitControl)}, 
-    {"FAULT_ACK", make_pair("archiver_command_abort", &SAL_archiver::ackCommand_abort)}
+template<class T> 
+class SAL { 
+    public: 
+    typedef salReturn (T::*funcptr)(int, salLONG, salLONG, char *); 
+
+    map<string, funcptr> action_handler = { 
+	{"START_ACK", &T::ackCommand_start}, 
+	{"STOP_ACK", &T::ackCommand_stop}, 
+	{"ENABLE_ACK", &T::ackCommand_enable}, 
+	{"DISABLE_ACK", &T::ackCommand_disable}, 
+	{"ENTERCONTROL_ACK", &T::ackCommand_enterControl}, 
+	{"STANDBY_ACK", &T::ackCommand_standby}, 
+	{"OFFLINE_ACK", &T::ackCommand_exitControl}, 
+	{"FAULT_ACK", &T::ackCommand_abort}
+    }; 
+};  
+
+struct visitor : public boost::static_visitor<> { 
+    template <typename T> 
+	void operator() (T &op) const { 
+	    SAL<T> t; 
+	    typename SAL<T>::funcptr action = t.action_handler[dict_key]; 
+	    op.salProcessor(const_cast<char *>(cmd.c_str())); 
+	    (op.*action)(cmdId, SAL__CMD_COMPLETE, error_code, const_cast<char *>(ack_statement.c_str()));
+	}
+	
+	string dict_key; 
+	string cmd; 
+	int cmdId; 
+	salLONG error_code; 
+	string ack_statement;  
 }; 
 
 map<string, map<string, string>> ack_book_keeper; 
@@ -51,20 +74,21 @@ void AckSubscriber::on_message(string message) {
 
     Node node = Load(message); 
     message_value = node["MSG_TYPE"].as<string>(); 
-    SAL_archiver mgr = SAL_archiver();
-
+    
     if (message_value == "BOOK_KEEPING") { 
 	string ack_delay = node["ACK_DELAY"].as<string>(); 
 	string ack_id = node["ACK_ID"].as<string>(); 
 	string time = node["TIME"].as<string>(); 
 	string checkbox = node["CHECKBOX"].as<string>(); 
 	string cmdId = node["CMD_ID"].as<string>(); 
+	string device = node["DEVICE"].as<string>(); 
 
 	map<string, string> innerdict; 
 	innerdict["ACK_DELAY"] = ack_delay; 
 	innerdict["TIME"] = time; 
 	innerdict["CHECKBOX"] = checkbox; 
 	innerdict["CMD_ID"] = cmdId; 
+	innerdict["DEVICE"] = device; 
 
 	ack_book_keeper[ack_id] = innerdict; 
 	cout << "BOOKKEEPING: " << cmdId << endl; 
@@ -72,13 +96,14 @@ void AckSubscriber::on_message(string message) {
     else if (message_value == "RESOLVE_ACK") { 
 	for (auto &ack_dict : ack_book_keeper) { 
 	    string check_box = ack_dict.second.find("CHECKBOX")->second; 
+	    string device = ack_dict.second.find("DEVICE")->second; 
 	    string ack_id = ack_dict.first; 
+
 	    if (check_box == "false") { 
 		string dict_time = ack_dict.second.find("TIME")->second; 
 		string ack_delay = ack_dict.second.find("ACK_DELAY")->second; 
 
 		int time_delta = get_time_delta(dict_time); 
-		cout << "DTIME: " << to_string(time_delta) << endl;
 		bool timeout_result = time_delta > stoi(ack_delay) ? true : false; 
 
 		if (timeout_result) { 
@@ -87,11 +112,18 @@ void AckSubscriber::on_message(string message) {
 		    
 		    string ack_id = ack_dict.first; 
 		    string construct_msg = ack_id.substr(0, ack_id.find("_")) + "_ACK";  
-		    funcptr action = get<1>(action_handler[construct_msg]); 
-		    string cmd = get<0>(action_handler[construct_msg]); 
+		    string cmd = get_salProcessor(device, ack_id); 
+		    
+		    visitor v; 
+		    v.dict_key = construct_msg; 
+		    v.cmd = cmd; 
+		    v.cmdId = command_id; 
+		    v.error_code = -302; 
+		    v.ack_statement = "DONE: OK"; 
 
-		    mgr.salProcessor(const_cast<char *>(cmd.c_str())); 
-		    (mgr.*action)(command_id, SAL__CMD_COMPLETE, 0, "DONE: OK"); 
+		    cout << "XXX PRINT: " << device << "::" << ack_id << "::" << cmd << "::" << v.ack_statement << endl; 
+		    sal_obj mgr = get_SALObj(device); 
+		    boost::apply_visitor(v, mgr); 
 
 		    ack_book_keeper[ack_id]["CHECKBOX"] = "true";
 		}  	
@@ -100,29 +132,48 @@ void AckSubscriber::on_message(string message) {
     } 
     else { 
 	cmdId = stoi(node["CMD_ID"].as<string>()); 
+	string device = node["DEVICE"].as<string>(); 
+	string ack_id = node["ACK_ID"].as<string>(); 
+	string ack_bool = node["ACK_BOOL"].as<string>(); 
+	string ack_statement = node["ACK_STATEMENT"].as<string>(); 
+	string cmd = get_salProcessor(device, ack_id); 
+
+	visitor v; 
+	v.dict_key = message_value; 
+	v.cmdId = cmdId; 
+	v.cmd = cmd; 
+	v.error_code = (ack_bool == "true") ? 0: -302; 
+	v.ack_statement = ack_statement; 
 	
-	string cmd = get<0>(action_handler[message_value]); 
-	if (!message_value.empty()) { 
-	    mgr.salProcessor(const_cast<char *>(cmd.c_str())); 
-	    funcptr action = get<1>(action_handler[message_value]); 
-
-	    string ack_bool = node["ACK_BOOL"].as<string>(); 
-	    string ack_statement = node["ACK_STATEMENT"].as<string>(); 
-	    
-	    if (ack_bool == "true") { 
-		(mgr.*action)(cmdId, SAL__CMD_COMPLETE, 0, const_cast<char *>(ack_statement.c_str()));     
-	    } 
-	    else { 
-		(mgr.*action)(cmdId, SAL__CMD_COMPLETE, -302, const_cast<char *>(ack_statement.c_str()));     
-	    }  	
-
-	    string ack_id = node["ACK_ID"].as<string>(); 
-	    ack_book_keeper[ack_id]["CHECKBOX"] = "true"; 
-	} 
-	else { 
-	    cout << "MSG_TYPE is not valid." << endl; 
-	} 
+	cout << "XXX PRINT2: " << cmdId << "::" << device << "::" << ack_id << "::" << cmd << "::" << v.ack_statement << endl; 
+	sal_obj mgr = get_SALObj(device); 
+	boost::apply_visitor(v, mgr); 
+	
+	ack_book_keeper[ack_id]["CHECKBOX"] = "true"; 
     } 
+} 
+
+string AckSubscriber::get_salProcessor(string device, string ack_id) { 
+    cout << "XXX GET_SAL: " << device << "::" << ack_id << endl; 
+    string command = ack_id.substr(0, ack_id.find("_")); 
+    transform(command.begin(), command.end(), command.begin(), ::tolower); 
+    string device_name; 
+	
+    if (device == "AR") { device_name = "archiver"; } 
+    else if (device == "CU") { device_name = "catchuparchiver"; } 
+    else { device_name = "processingcluster"; } 
+
+    string cmd = device_name + "_command_" + command; 
+
+    return cmd; 
+} 
+
+AckSubscriber::sal_obj AckSubscriber::get_SALObj(string device) { 
+    sal_obj my_device; 
+    if (device == "AR") my_device = SAL_archiver(); 
+    else if (device == "CU") my_device = SAL_catchuparchiver(); 
+    else my_device = SAL_processingcluster(); 
+    return my_device;  
 } 
 
 int main() { 
