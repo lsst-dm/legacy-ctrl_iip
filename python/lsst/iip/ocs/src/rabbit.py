a25127b1fc3fa756ac8107a42300f7321fd0321c
hwin16 <htutkhwin@gmail.com>
2017-04-12 19:15:57 +0000

    Rabbit messages for test updated.

--- python/lsst/iip/ocs/src/rabbit.py
+++ python/lsst/iip/ocs/src/rabbit.py
@@ -13,110 +13,92 @@ logging.basicConfig()
 sp = SimplePublisher("amqp://OCS:OCS@141.142.208.191:5672/%2fbunny")
 
 m = {} 
-m["MSG_TYPE"] = "SUMMARY_STATE"
+m["MSG_TYPE"] = "SUMMARY_STATE_EVENT"
 m["DEVICE"] = "AR"
-m["NAME"] = "HELLO"
-m["IDENTIFIER"] = "0"
-m["TIMESTAMP"] = "HELLO"
-m["ADDRESS"] = "1" 
-m["CURRENT_STATE"] = "ENABLE"
-m["PREVIOUS_STATE"] = "DISABLE"
-m["EXECUTING"] = "NOW"
-m["COMMANDS_AVAILABLE"] = "A,B"
-m["CONFIGURATIONS_AVAILABLE"] = "C,D"
-m["PRIORITY"] = "2"
+m["CURRENT_STATE"] = "0"
 sp.publish_message("dmcs_ocs_publish", m)
 
 m2 = {} 
-m2["MSG_TYPE"] = "SUMMARY_STATE"
+m2["MSG_TYPE"] = "SUMMARY_STATE_EVENT"
 m2["DEVICE"] = "CU"
-m2["NAME"] = "HELLO"
-m2["IDENTIFIER"] = "0"
-m2["TIMESTAMP"] = "HELLO"
-m2["ADDRESS"] = "1" 
-m2["CURRENT_STATE"] = "ENABLE"
-m2["PREVIOUS_STATE"] = "DISABLE"
-m2["EXECUTING"] = "NOW"
-m2["COMMANDS_AVAILABLE"] = "A,B"
-m2["CONFIGURATIONS_AVAILABLE"] = "C,D"
-m2["PRIORITY"] = "2"
+m2["CURRENT_STATE"] = "0"
 sp.publish_message("dmcs_ocs_publish", m2)
 
 m3 = {} 
-m3["MSG_TYPE"] = "SUMMARY_STATE"
+m3["MSG_TYPE"] = "SUMMARY_STATE_EVENT"
 m3["DEVICE"] = "PP"
-m3["NAME"] = "HELLO"
-m3["IDENTIFIER"] = "0"
-m3["TIMESTAMP"] = "HELLO"
-m3["ADDRESS"] = "1" 
-m3["CURRENT_STATE"] = "ENABLE"
-m3["PREVIOUS_STATE"] = "DISABLE"
-m3["EXECUTING"] = "NOW"
-m3["COMMANDS_AVAILABLE"] = "A,B"
-m3["CONFIGURATIONS_AVAILABLE"] = "C,D"
-m3["PRIORITY"] = "2"
+m3["CURRENT_STATE"] = "0"
 sp.publish_message("dmcs_ocs_publish", m3)
 
 m4 = {} 
 m4["ERROR_CODE"] = "1" 
-m4["PRIORITY"] = "2"
-m4["MSG_TYPE"] = "ERROR_CODE"
+m4["MSG_TYPE"] = "ERROR_CODE_EVENT"
 m4["DEVICE"] = "AR"
 sp.publish_message("dmcs_ocs_publish", m4) 
 
 
 m5 = {} 
 m5["ERROR_CODE"] = "1" 
-m5["PRIORITY"] = "2"
-m5["MSG_TYPE"] = "ERROR_CODE"
+m5["MSG_TYPE"] = "ERROR_CODE_EVENT"
 m5["DEVICE"] = "CU"
 sp.publish_message("dmcs_ocs_publish", m5) 
 
 m6 = {} 
 m6["ERROR_CODE"] = "1" 
-m6["PRIORITY"] = "2"
-m6["MSG_TYPE"] = "ERROR_CODE"
+m6["MSG_TYPE"] = "ERROR_CODE_EVENT"
 m6["DEVICE"] = "PP"
 sp.publish_message("dmcs_ocs_publish", m6) 
 
 m7 = {} 
-m7["APPLIED_SETTINGS_MATCHSTART_ISTRUE"] = "true"
-m7["PRIORITY"] = "2"
+m7["BOOL"] = "true"
 m7["DEVICE"] = "AR"
-m7["MSG_TYPE"] = "APPLIED_SETTINGS_MATCHSTART"
+m7["MSG_TYPE"] = "APPLIED_SETTINGS_MATCH_START_EVENT"
 sp.publish_message("dmcs_ocs_publish", m7) 
 
 m8 = {} 
-m8["APPLIED_SETTINGS_MATCHSTART_ISTRUE"] = "false"
-m8["PRIORITY"] = "2"
+m8["BOOL"] = "false"
 m8["DEVICE"] = "CU"
-m8["MSG_TYPE"] = "APPLIED_SETTINGS_MATCHSTART"
+m8["MSG_TYPE"] = "APPLIED_SETTINGS_MATCH_START_EVENT"
 sp.publish_message("dmcs_ocs_publish", m8) 
 
 m9 = {} 
-m9["APPLIED_SETTINGS_MATCHSTART_ISTRUE"] = "true"
-m9["PRIORITY"] = "2"
+m9["BOOL"] = "true"
 m9["DEVICE"] = "PP"
-m9["MSG_TYPE"] = "APPLIED_SETTINGS_MATCHSTART"
+m9["MSG_TYPE"] = "APPLIED_SETTINGS_MATCH_START_EVENT"
 sp.publish_message("dmcs_ocs_publish", m9) 
 
 m10 = {} 
-m10["MSG_TYPE"] = "RECOMMENDED_SETTING_VERSIONS" 
+m10["MSG_TYPE"] = "RECOMMENDED_SETTINGS_VERSION_EVENT" 
 m10["DEVICE"] = "AR"
-m10["PRIORITY"] = "2" 
-m10["RECOMMENDED_SETTING_VERSION"] = "2"
+m10["CFG_KEY"] = "AR-normal"
 sp.publish_message("dmcs_ocs_publish", m10) 
 
 m11 = {} 
-m11["MSG_TYPE"] = "RECOMMENDED_SETTING_VERSIONS" 
+m11["MSG_TYPE"] = "RECOMMENDED_SETTINGS_VERSION_EVENT" 
 m11["DEVICE"] = "CU"
-m11["PRIORITY"] = "2" 
-m11["RECOMMENDED_SETTING_VERSION"] = "2"
+m11["CFG_KEY"] = "CU-Normal"
 sp.publish_message("dmcs_ocs_publish", m11) 
 
 m12 = {} 
-m12["MSG_TYPE"] = "RECOMMENDED_SETTING_VERSIONS" 
+m12["MSG_TYPE"] = "RECOMMENDED_SETTINGS_VERSION_EVENT" 
 m12["DEVICE"] = "PP"
-m12["PRIORITY"] = "2" 
-m12["RECOMMENDED_SETTING_VERSION"] = "2"
+m12["CFG_KEY"] = "PP-Normal"
 sp.publish_message("dmcs_ocs_publish", m12) 
+
+m13 = {} 
+m13["MSG_TYPE"] = "SETTINGS_APPLIED_EVENT"
+m13["DEVICE"] = "AR" 
+m13["SETTING"] = "NORMAL" 
+sp.publish_message("dmcs_ocs_publish", m13) 
+
+m14 = {} 
+m14["MSG_TYPE"] = "SETTINGS_APPLIED_EVENT"
+m14["DEVICE"] = "CU" 
+m14["SETTING"] = "NORMAL" 
+sp.publish_message("dmcs_ocs_publish", m14) 
+
+m15 = {} 
+m15["MSG_TYPE"] = "SETTINGS_APPLIED_EVENT"
+m15["DEVICE"] = "AR" 
+m15["SETTING"] = "NORMAL" 
+sp.publish_message("dmcs_ocs_publish", m15) 
