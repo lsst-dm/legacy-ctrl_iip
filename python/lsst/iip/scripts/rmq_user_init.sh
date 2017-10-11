37027ed398d8901b52c18846b11d629a9632c870
Jim Parsons <jbparson@illinois.edu>
2017-10-11 10:28:07 -0500

    Adding more users to set up script for message broker

--- python/lsst/iip/scripts/rmq_user_init.sh
+++ python/lsst/iip/scripts/rmq_user_init.sh
@@ -1,4 +1,4 @@
-rabbitmqctl add_vhost /bunny
+#rabbitmqctl add_vhost /bunny
 
 rabbitmqctl add_user F1 F1
 rabbitmqctl add_user F2 F2
@@ -57,6 +57,77 @@ rabbitmqctl set_user_tags FM administrator
 rabbitmqctl add_user DMCS DMCS
 rabbitmqctl set_permissions -p /bunny DMCS '.*' '.*' '.*'
 rabbitmqctl set_user_tags DMCS administrator
+
+
+rabbitmqctl add_user AFM AFM
+rabbitmqctl set_permissions -p /bunny AFM '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test AFM '.*' '.*' '.*'
+
+rabbitmqctl add_user AFM_PUB AFM_PUB
+rabbitmqctl set_permissions -p /bunny AFM_PUB '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test AFM_PUB '.*' '.*' '.*'
+
+rabbitmqctl add_user PFM PFM
+rabbitmqctl set_permissions -p /bunny PFM '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test PFM '.*' '.*' '.*'
+
+rabbitmqctl add_user PFM_PUB PFM_PUB
+rabbitmqctl set_permissions -p /bunny PFM_PUB '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test PFM_PUB '.*' '.*' '.*'
+
+rabbitmqctl add_user PFM_NCSA PFM_NCSA
+rabbitmqctl set_permissions -p /bunny PFM_NCSA '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test PFM_NCSA '.*' '.*' '.*'
+
+rabbitmqctl add_user PFM_NCSA_PUB PFM_NCSA_PUB
+rabbitmqctl set_permissions -p /bunny PFM_NCSA_PUB '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test PFM_NCSA_PUB '.*' '.*' '.*'
+
+rabbitmqctl add_user NFM_BASE NFM_BASE
+rabbitmqctl set_permissions -p /bunny NFM_BASE '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test NFM_BASE '.*' '.*' '.*'
+
+rabbitmqctl add_user NFM_BASE_PUB NFM_BASE_PUB
+rabbitmqctl set_permissions -p /bunny NFM_BASE_PUB '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test NFM_BASE_PUB '.*' '.*' '.*'
+
+rabbitmqctl add_user NFM_NCSA NFM_NCSA
+rabbitmqctl set_permissions -p /bunny NFM_NCSA '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test NFM_NCSA '.*' '.*' '.*'
+
+rabbitmqctl add_user NFM_NCSA_PUB NFM_NCSA_PUB
+rabbitmqctl set_permissions -p /bunny NFM_NCSA_PUB '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test NFM_NCSA_PUB '.*' '.*' '.*'
+
+rabbitmqctl add_user ARCHIE ARCHIE
+rabbitmqctl set_permissions -p /bunny ARCHIE '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test ARCHIE '.*' '.*' '.*'
+
+rabbitmqctl add_user ARCHIE_PUB ARCHIE_PUB
+rabbitmqctl set_permissions -p /bunny ARCHIE_PUB '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test ARCHIE_PUB '.*' '.*' '.*'
+
+rabbitmqctl add_user DMCS DMCS
+rabbitmqctl set_permissions -p /bunny DMCS '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test DMCS '.*' '.*' '.*'
+
+rabbitmqctl add_user DMCS_PUB DMCS_PUB
+rabbitmqctl set_permissions -p /bunny DMCS_PUB '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test DMCS_PUB '.*' '.*' '.*'
+
+rabbitmqctl add_user AUDIT AUDIT
+rabbitmqctl set_permissions -p /bunny AUDIT '.*' '.*' '.*'
+rabbitmqctl set_permissions -p /test AUDIT '.*' '.*' '.*'
+
+
+for i in {1..25}; do 
+   x =  "CL"$i; 
+   rabbitmqctl add_user $x $x;
+   rabbitmqctl set_permissions -p /test $x \'.\*\' \'.\*\' \'.\*\'
+   rabbitmqctl set_permissions -p /bunny $x \'.\*\' \'.\*\' \'.\*\'
+   done
+
+
 
 
 
