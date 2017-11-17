4b18b0910ef190cd13e21008640c31ac424cf486
a1b3rtlim72 <zero72x@yahoo.com>
2017-11-17 21:51:56 +0000

    Changed tabs to space. Getting ready to move machines

--- python/lsst/iip/BaseMgmt.py
+++ python/lsst/iip/BaseMgmt.py
@@ -2,10 +2,10 @@ from time import sleep
 
 class BaseMgmt:
 
-	def __init__(self):
+    def __init__(self):
 
 
-	def ack_timer(self, seconds):
-		sleep(seconds)
-		return True
+    def ack_timer(self, seconds):
+        sleep(seconds)
+        return True
 
