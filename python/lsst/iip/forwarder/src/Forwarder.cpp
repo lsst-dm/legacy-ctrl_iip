cf12fc42b2bdba20ccd4dc4b03307c0862d06501
Jim Parsons <jbparson@illinois.edu>
2017-12-03 11:37:01 -0600

    Genesis of pure messaging version

---
+++ python/lsst/iip/forwarder/src/Forwarder.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <sstream>
+#include <pthread.h>
+#include <yaml-cpp/yaml.h>
+#include "Consumer_impl.h"
+#include "SimplePublisher.h"
+
+using namespace std;
+using namespace YAML;
+
+class Forwarder {
+    public:
+    //General Forwarder consumers
+    Consumer *from_foreman_consumer;
+    Consumer *from_fetch_consumer;
+    Consumer *from_format_consumer;
+    Consumer *from_forward_consumer;
+
+    //Consumers in work threads
+    Consumer *from_forwarder_to_fetch;
+    Consumer *from_forwarder_to_format;
+    Consumer *from_forwarder_to_forward;
+
+    //Publishers
+    SimplePublisher *FWDR_pub;
+    SimplePublisher *fetch_pub;
+    SimplePublisher *fmt_pub;
+    SimplePublisher *fwd_pub;
+    
+
+    Forwarder();
+    ~Forwarder();
+    void setup_consumers();
+    void on_foreman_message(string body);
+    void on_fetch_message(string body);
+    void on_format_message(string body);
+    void on_forward_message(string body);
+
+    void on_forwarder_to_fetch_message(string body);
+    void on_forwarder_to_format_message(string body);
+    void on_forwarder_to_forward_message(string body);
+
+    void run();
+    statis void *run_thread(void *);
+};
+
+using funcptr = void(Forwarder::*)(Node);
+
+//Primary Forwarder message actions
+map<string, funcptr> on_foreman_message_actions = {
+
+};
+
+map<string, funcptr> on_fetch_message_actions = {
+
+};
+
+map<string, funcptr> on_format_message_actions = {
+
+};
+
+map<string, funcptr> on_forward_message_actions = {
+
+};
+
+
+//Forwarder Component message actions
+map<string, funcptr> on_forwarder_to_fetch_message_actions = {
+
+};
+
+map<string, funcptr> on_forwarder_to_format_message_actions = {
+
+};
+
+map<string, funcptr> on_forwarder_to_forward_message_actions = {
+
+};
+
+//Messages received by Primary Forwarder from Foreman
+void Forwarder::on_foreman_message(string body) {
+    Node node = Load(body);
+    string message_type = node["MSG_TYPE"].as<string>();
+    funcptr action = on_foreman_message_actions[message_type];
+    (this->*action)(node);
+}
+
+//Messages received by Primary Forwarder from fetch thread
+void Forwarder::on_fetch_message(string body) {
+    Node node = Load(body);
+    string message_type = node["MSG_TYPE"].as<string>();
+    funcptr action = on_fetch_message_actions[message_type];
+    (this->*action)(node);
+}
+
+//Messages received by Primary Forwarder from format thread
+void Forwarder::on_format_message(string body) {
+    Node node = Load(body);
+    string message_type = node["MSG_TYPE"].as<string>();
+    funcptr action = on_format_message_actions[message_type];
+    (this->*action)(node);
+}
+
+//Messages received by Primary Forwarder from forwardthread
+void Forwarder::on_forward_message(string body) {
+    Node node = Load(body);
+    string message_type = node["MSG_TYPE"].as<string>();
+    funcptr action = on_forward_message_actions[message_type];
+    (this->*action)(node);
+}
+
+
+
+//Messages received by the fetch, format, and forward threads
+void Forwarder::on_forwarder_to_fetch_message(string body) {
+    Node node = Load(body);
+    string message_type = node["MSG_TYPE"].as<string>();
+    funcptr action = on_forward_to_fetch_message_actions[message_type];
+    (this->*action)(node);
+}
+
+void Forwarder::on_forwarder_to_format_message(string body) {
+    Node node = Load(body);
+    string message_type = node["MSG_TYPE"].as<string>();
+    funcptr action = on_forwarder_to format_message_actions[message_type];
+    (this->*action)(node);
+}
+
+void Forwarder::on_forwarder_to_forward_message(string body) {
+    Node node = Load(body);
+    string message_type = node["MSG_TYPE"].as<string>();
+    funcptr action = on_forwarder_to_forward_message_actions[message_type];
+    (this->*action)(node);
+}
+
+
+
+
+
+
+
+
+
