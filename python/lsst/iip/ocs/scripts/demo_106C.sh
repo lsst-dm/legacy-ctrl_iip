8ee7113bc2fe57aa43a7fb5c4b083f322fcbdde2
Htut Khine Win <hwin16@illinois.edu>
2018-01-26 15:44:00 -0600

    demo script added.

---
+++ python/lsst/iip/ocs/scripts/demo_106C.sh
@@ -0,0 +1,8 @@
+echo "Initiating Sequence..." 
+./emu_NextVisit.sh 1 2
+sleep 20
+echo "[OK] NEXT_VISIT SEQUENCE COMPLETE." 
+
+./emu_NextVisit.sh 1 5
+sleep 20 
+echo "[OK] NEXT_VISIT SEQUENCE COMPLETE." 
