9e745de5fe09f16851b7c1555ccd920c14474513
Htut Khine <hwin16@illinois.edu>
2018-04-03 07:27:37 -0500

    Fixed format,fetch in DAQForwarder.

--- python/lsst/iip/fmt_sender.py
+++ python/lsst/iip/fmt_sender.py
@@ -3,13 +3,14 @@ from time import sleep
 
 
 sp = SimplePublisher("amqp://PFM_PUB:PFM_PUB@141.142.238.10/%2ftest", "YAML") 
-
+'''
 msg = {} 
 msg["MSG_TYPE"] = "AT_FWDR_HEADER_READY" 
 msg["IMAGE_ID"] = "IMG_100"
 msg["FILENAME"] = "dmills@141.142.238.176:/tmp/cam.header"
 sp.publish_message("f1_consume", msg) 
 sleep(1)
+'''
 
 msg = {} 
 msg["MSG_TYPE"] = "FORMAT_HEADER_READY" 
--- python/lsst/iip/ocs/scripts/demo_106C.sh
+++ python/lsst/iip/ocs/scripts/demo_106C.sh
@@ -26,12 +26,12 @@ sleep 3
 echo -e "[${GREEN}${BOLD}OK${NOCOL}] START_INTEGRATION sent."
 
 # efd
-../events/sacpp_efd_LargeFileObjectAvailable_send 0 a AT a dmills@141.142.238.176:/tmp/cam.header 0 IMG_100 0 
-sleep 1
-echo -e "[${GREEN}${BOLD}OK${NOCOL}] HEADER_READY sent."
+#../events/sacpp_efd_LargeFileObjectAvailable_send 0 a AT a dmills@141.142.238.176:/tmp/cam.header 0 IMG_100 0 
+#sleep 1
+#echo -e "[${GREEN}${BOLD}OK${NOCOL}] HEADER_READY sent."
 
 # endReadout
-../events/sacpp_atcamera_endReadout_send seq_123 1 IMG_100 1 0 0 0
+../events/sacpp_atcamera_endReadout_send seq_123 1 LSSTTEST-1 1 0 0 0
 sleep 1
 echo -e "[${GREEN}${BOLD}OK${NOCOL}] END_READOUT sent."
 
