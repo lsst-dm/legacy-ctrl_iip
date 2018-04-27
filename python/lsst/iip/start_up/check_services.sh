2d480cd107eb428fa25cb52fe1f39102a597c0d8
Htut Khine Win <hwin16@illinois.edu>
2018-04-27 19:31:53 +0000

    Updated ctrl_check dependency.

--- python/lsst/iip/start_up/check_services.sh
+++ python/lsst/iip/start_up/check_services.sh
@@ -15,7 +15,15 @@ while true; do
                 echo "${sv} is active." 
                 ;;
             *)
+                echo "Stopping services..."
+                systemctl stop DMCS
+                systemctl stop AuxDevice
+                systemctl stop CommandListener
+                systemctl stop EventSubscriber
+                systemctl stop AckSubscriber
+                echo "Purging queues..."
                 /usr/bin/bash ./scripts/at_rmq_purge_queues.sh
+                echo "Restarting services..."
                 systemctl restart ctrl_iip.target
                 sleep 10
                 ;; 
