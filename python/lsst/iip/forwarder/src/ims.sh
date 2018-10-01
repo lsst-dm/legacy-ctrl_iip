f943f2bf10cbef3cac19b9bb0ff5c4f139e20fbb
Jim Parsons <jbparson@illinois.edu>
2018-10-01 06:29:39 -0700

    Added explanation header and eliminated some cruft.

--- python/lsst/iip/forwarder/src/ims.sh
+++ python/lsst/iip/forwarder/src/ims.sh
@@ -1,3 +1,16 @@
+######################################################################################
+# NOTE: This script is HIGHLY temprary. Its purpose is simple:
+# It calls the ims_list DAQ example application, which dumps all of the current 
+# images in the catalog and all ancillary info.
+# It then separates column 7 (the actual name of the image) into a list,
+# and checks this list for the incoming image name argument - then
+# returns an exit code for either success (0) or failure.
+# The way it is currently built, it is hardcoded to look strictly for
+# raft partition 'ats'
+#
+# It is called by the Forwarder at runtime.
+
+
 /srv/nfs/lsst-daq/daq-sdk/R2-V1.14/x86/bin/ims_list ats |awk -F " " '{print $NF}'|grep $1
 #echo "Result is: $?"
 if [ $? = 1 ]
@@ -8,8 +21,3 @@ if [ $? = 1 ]
 fi
 #echo $?
 
-
-#/srv/nfs/lsst-daq/daq-sdk/R2-V1.14/x86/bin/ims_list ats |grep $1
-#X=$(/srv/nfs/lsst-daq/daq-sdk/R2-V1.14/x86/bin/ims_list ats |awk -F " " '{print $NF}'|grep $1)
-#X=$(/srv/nfs/lsst-daq/daq-sdk/R2-V1.14/x86/bin/ims_list ats |grep $1)
-#X=$(ims ats |awk -F " " '{print $NF}' |grep $1)
