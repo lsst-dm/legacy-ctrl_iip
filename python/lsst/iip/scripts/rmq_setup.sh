ce49f9ceddcc91d8caf5cc58c6d2f1a197004da3
Htut Khine <htutkhwin@gmail.com>
2019-03-07 02:52:36 +0630

    Moved scripts and service files.

--- python/lsst/iip/scripts/rmq_setup.sh
+++
@@ -1,2 +0,0 @@
-rabbitmq-plugins enable rabbitmq_management
-rabbitmqctl set_user_tags DMCS "management"
--- python/lsst/iip/start_up/test_pika.py
+++
@@ -1,3 +0,0 @@
-import pika 
-
-co = pika.BlockingConnection(pika.URLParameters("amqp://DMCS:DMCS@141.142.208.160:5672/%2fbunny_at"))
