bbf8ede849e81a2b60b31336f78082daf73ce957
Htut Khine <htutkhwin@gmail.com>
2019-03-07 02:36:34 +0630

    Cleaned up directories to conform to DM standard.

--- python/lsst/iip/AuditWeb/messages/display/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from .models import Machines
-
-# Register your models here.
-admin.site.register(Machines)
