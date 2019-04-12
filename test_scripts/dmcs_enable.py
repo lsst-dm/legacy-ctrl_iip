da0219ff0ab6348cd0754c7c71e726246b5de0b8
Stephen R Pietrowicz <srp@illinois.edu>
2019-04-12 14:31:28 -0500

    Change to make rabbitmq connections have one user and one passwd, instead of many of each

--- test_scripts/dmcs_enable.py
+++ test_scripts/dmcs_enable.py
@@ -1,75 +1,49 @@
 import pika
-#from FirehoseConsumer import FirehoseConsumer
-from lsst.ctrl.iip.Consumer import Consumer
 from lsst.ctrl.iip.SimplePublisher import SimplePublisher
+from lsst.ctrl.iip.Credentials import Credentials
 import sys
 import os
 import time
 import logging
-import _thread
 
-class Premium:
-  def __init__(self):
-    logging.basicConfig()
-    # commented out - srp
-    #broker_url = 'amqp://FORWARD_F93:FORWARD_F93@140.252.32.128:5672/%2Ftest_at'
-    broker_url = 'amqp://FORWARD_F93:FORWARD_F93@141.142.238.10:5672/%2Ftest_at_srp'
+if __name__ == "__main__":
+    cred = Credentials('iip_cred.yaml')
+    user = cred.getUser('service_user')
+    passwd = cred.getPasswd('service_passwd')
+  
+    url = 'amqp://%s:%s@141.142.238.10:5672/%%2Ftest_at' % (user, passwd)
+    sp1 = SimplePublisher(url, "YAML")
 
-    #self.new_thread = Consumer(broker_url, 'telemetry_queue', 'xthread', self.mycallback, 'YAML')
-    #self.new_thread.start()
+    """
+    """
+    msg = {}
+    msg['MSG_TYPE'] = "STANDBY"
+    msg['DEVICE'] = 'AT'
+    #msg['CFG_KEY'] = "2C16"
+    msg['ACK_ID'] = 'AT_4'
+    msg['CMD_ID'] = '4434278812'
+    time.sleep(3)
+    print("AT STANDBY")
+    sp1.publish_message("ocs_dmcs_consume", msg)
 
-    #cdm = toolsmod.intake_yaml_file("L1SystemCfg.yaml")
-    #self.ccd_list = cdm['ROOT']['CCD_LIST']
-   
-  def mycallback(self, ch, method, properties, body):
-    ch.basic_ack(method.delivery_tag)
-    print("  ")
-    print(">>>>>>>>>>>>>>><<<<<<<<<<<<<<<<")
-    print((" [z] body Received %r" % body))
-    print(">>>>>>>>>>>>>>><<<<<<<<<<<<<<<<")
+    msg = {}
+    msg['MSG_TYPE'] = "DISABLE"
+    msg['DEVICE'] = 'AT'
+    msg['ACK_ID'] = 'AT_6'
+    msg['CMD_ID'] = '4434278814'
+    time.sleep(3)
+    print("AT DISABLE")
+    sp1.publish_message("ocs_dmcs_consume", msg)
 
-    #print("Message done")
-    #print("Still listening...")
+    msg = {}
+    msg['MSG_TYPE'] = "ENABLE"
+    msg['DEVICE'] = 'AT'
+    msg['ACK_ID'] = 'AT_11'
+    msg['CMD_ID'] = '4434278816'
+    time.sleep(3)
+    print("AT ENABLE")
+    sp1.publish_message("ocs_dmcs_consume", msg)
 
-
-def main():
-  # commented out - srp
-  #sp1 = SimplePublisher('amqp://DMCS:DMCS@140.252.32.128:5672/%2Ftest_at', "YAML")
-  sp1 = SimplePublisher('amqp://DMCS:DMCS@141.142.238.10:5672/%2Ftest_at', "YAML")
-
-  #while 1:
-    #pass
-  """
-  """
-  msg = {}
-  msg['MSG_TYPE'] = "STANDBY"
-  msg['DEVICE'] = 'AT'
-  #msg['CFG_KEY'] = "2C16"
-  msg['ACK_ID'] = 'AT_4'
-  msg['CMD_ID'] = '4434278812'
-  time.sleep(3)
-  print("AT STANDBY")
-  sp1.publish_message("ocs_dmcs_consume", msg)
-
-  msg = {}
-  msg['MSG_TYPE'] = "DISABLE"
-  msg['DEVICE'] = 'AT'
-  msg['ACK_ID'] = 'AT_6'
-  msg['CMD_ID'] = '4434278814'
-  time.sleep(3)
-  print("AT DISABLE")
-  sp1.publish_message("ocs_dmcs_consume", msg)
-
-  msg = {}
-  msg['MSG_TYPE'] = "ENABLE"
-  msg['DEVICE'] = 'AT'
-  msg['ACK_ID'] = 'AT_11'
-  msg['CMD_ID'] = '4434278816'
-  time.sleep(3)
-  print("AT ENABLE")
-  sp1.publish_message("ocs_dmcs_consume", msg)
-
-  sp1.close()
+    sp1.close()
 
 
-if __name__ == "__main__":  main()
