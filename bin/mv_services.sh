444e7c2531191b731d31b4859d7b034225511a54
Htut Khine Htay Win <hwin16@illinois.edu>
2019-05-02 08:44:42 -0500

    Updated paths in systemd unit files

--- bin/mv_services.sh
+++ bin/mv_services.sh
@@ -4,6 +4,7 @@ case $1 in
         cp ../etc/services/*.service /etc/systemd/system
         cp ../etc/services/*.target /etc/systemd/system
         systemctl daemon-reload
+	systemctl start l1d-ctrl_iip.target
         ;;
     *)
         cp $1 /etc/systemd/system
